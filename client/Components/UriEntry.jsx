a3a6b440b46aa2fd5def6d13411da2e15ebf40ff
Adam Goodman <adamrgoodman1@gmail.com>
2021-09-08 12:36:43 -0400

    completed route integration tests. Started work on invalid post requests

--- client/Components/UriEntry.jsx
+++ client/Components/UriEntry.jsx
@@ -23,26 +23,6 @@ const styles = {
   },
 };
 
-export default function UriEntry(props) {
-  console.log(props.gTD);
-
-  const classes = useStyles();
-  return (
-    <div id="uri-entry">
-      <form id="uribox" noValidate autoComplete="off">
-        <TextField
-          className={classes.root}
-          id="filled-basic"
-          onBlur={() => props.gTD()}
-          InputProps={{ className: classes.input }}
-          label="Enter database URI"
-          variant="filled"
-        />
-        <Link to="/visualize">
-          <Button className={classes.root} variant="contained" size="small">
-            Submit
-          </Button>
-        </Link>
 
 export default function UriEntry(props) {   
     const classes = useStyles();
