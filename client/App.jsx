f6ed9997beb51c841fca8446b795a198e49e789f
Mark <markyliu1@gmail.com>
2021-09-07 23:10:58 -0400

    removed unused components, other client files, deleted process.env file

--- client/App.jsx
+++ client/App.jsx
@@ -83,7 +83,7 @@ class App extends Component {
             <img id="logo" src="./assets/logo.png" />
           </Link>
           <div className="topButtons">
-            <Link to="/signup">
+            {/* <Link to="/signup">
               <button
                 className="signup-btn"
                 // onClick={(event) => {
@@ -94,7 +94,7 @@ class App extends Component {
                 {' '}
                 Sandbox{' '}
               </button>
-            </Link>
+            </Link> */}
 
             <Link to="/docs">
               <button className="login-btn"> Docs </button>
