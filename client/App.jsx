831fb96f9b9628c1252b2139955bcb9da70bed3c
Chris K <Chris-SourceCode@outlook.com>
2021-09-09 21:13:24 +0100

    feat: App - implement hooks

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Image, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import UriEntry from './Components/UriEntry.jsx';
@@ -15,25 +15,18 @@ import Team from './Components/Team.jsx';
 //   |Codebox
 //   |Diagram
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      database: {
-        allTables: {},
+function App() {
+  const [database, setDatabase] = useState({
+    allTables: {},
 
-        foreignKeys: [],
-        primaryKeys: [],
-        completeSchemaString: ' ',
-        resolvers: ' '
-      },
-      renderSchema: true,
-    };
-    this.gTD = this.gTD.bind(this);
-    this.changeRender = this.changeRender.bind(this);
-  }
+    foreignKeys: [],
+    primaryKeys: [],
+    completeSchemaString: ' ',
+    resolvers: ' ',
+  });
+  const [renderSchema, setRenderSchema] = useState(true);
 
-  gTD() {
+  const gTD = () => {
     axios
       .post('/api/uri', {
         uri:
@@ -43,100 +36,89 @@ class App extends Component {
       .then((response) => {
         // handle success
         document.getElementById('filled-basic').value = '';
-        this.setState(state => {
-          return {...state, database: response.data }
-        });
-        document.getElementById('outlined-multiline-static').value = this.state.database.completeSchemaString;
+        setDatabase(response.data);
+        document.getElementById('outlined-multiline-static').value =
+          database.completeSchemaString;
       })
-      .catch(function (error) {
+      .catch((error) => {
         // handle error
+
         console.log(error);
       });
-  }
+  };
 
-  changeRender(schema) {
+  const changeRender = (schema) => {
     if (schema === false) {
-      this.setState(state => {
-        return {...state, renderSchema: false}
-      })
-      document.getElementById('outlined-multiline-static').value = this.state.database.resolvers;
+      setRenderSchema(false);
+      document.getElementById('outlined-multiline-static').value =
+        database.resolvers;
     } else {
-      this.setState(state => {
-        return {...state, renderSchema: true}
-      })
-      document.getElementById('outlined-multiline-static').value = this.state.database.completeSchemaString;
+      setRenderSchema(true);
+      document.getElementById('outlined-multiline-static').value =
+        database.completeSchemaString;
     }
-    
+  };
 
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="header">
-          <Link to="/">
-            <img id="logo" src="./assets/logo.png" />
+  return (
+    <Router>
+      <div className='header'>
+        <Link to='/'>
+          <img id='logo' src='./assets/logo.png' />
+        </Link>
+        <div className='topButtons'>
+          <Link to='/docs'>
+            <button className='login-btn'> Docs </button>
           </Link>
-          <div className="topButtons">
-            {/* <Link to="/signup">
-              <button
-                className="signup-btn"
-                // onClick={(event) => {
-                //   console.log('trying to send you to singup');
-                //   console.log(this.state);
-                //   this.setState({ currentpage: 'signup' });}}
-              >
-                {' '}
-                Sandbox{' '}
-              </button>
-            </Link> */}
 
-            <Link to="/docs">
-              <button className="login-btn"> Docs </button>
-            </Link>
-
-            <a href="https://github.com/oslabs-beta/beam-corp" target="_blank" rel="noreferrer">
-              <button>Github</button>
-            </a>
-            <Link to="/team">
-              <button>Team</button>
-            </Link>
-          </div>
-          <ThemeProvider theme={theme}>
-            <UriEntry gTD={this.gTD} />
-          </ThemeProvider>
+          <a
+            href='https://github.com/oslabs-beta/beam-corp'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button>Github</button>
+          </a>
+          <Link to='/team'>
+            <button>Team</button>
+          </Link>
         </div>
+        <ThemeProvider theme={theme}>
+          <UriEntry gTD={gTD} />
+        </ThemeProvider>
+      </div>
 
-        <Switch>
-          <Route exact path="/">
-            <img id="gif" src="./assets/getstarted.gif" alt="Get Started" />
-            {/* <h1 id="gifheader">Getting Started</h1> */}
-          </Route>
-          <Route exact path="/docs">
-            <Docs />
-            <img
-              id="gif"
-              style={{ marginRight: '30px', width: '50vw' }}
-              src="./assets/getstarted.gif"
-              alt="Get Started"
-            />
-          </Route>
-          <Route path="/visualize">
-            <div id="OutputBox">
-              <Diagram id="outputRight" data={this.state.database} />
-              <div id="outputLeft">
-                <CodeOutputButtons database={this.state.database} renderSchema={this.state.renderSchema} changeRender={this.changeRender} />
-                <CodeOutput database={this.state.database} renderSchema={this.state.renderSchema} />
-              </div>
+      <Switch>
+        <Route exact path='/'>
+          <img id='gif' src='./assets/getstarted.gif' alt='Get Started' />
+          {/* <h1 id="gifheader">Getting Started</h1> */}
+        </Route>
+        <Route exact path='/docs'>
+          <Docs />
+          <img
+            id='gif'
+            style={{ marginRight: '30px', width: '50vw' }}
+            src='./assets/getstarted.gif'
+            alt='Get Started'
+          />
+        </Route>
+        <Route path='/visualize'>
+          <div id='OutputBox'>
+            <Diagram id='outputRight' data={database} />
+            <div id='outputLeft'>
+              <CodeOutputButtons
+                database={database}
+                renderSchema={renderSchema}
+                changeRender={changeRender}
+              />
+              <CodeOutput database={database} renderSchema={renderSchema} />
             </div>
-          </Route>
-          <Route exact path="/team">
-            <Team />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+          </div>
+        </Route>
+        <Route exact path='/team'>
+          <Team />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
