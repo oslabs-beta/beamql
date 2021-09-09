adad08749b806702ef09d0fb8cbd480027dc416b
Brian Grosso <bgro63@gmail.com>
2021-09-09 16:24:03 -0400

    docs formatting updates

--- client/Components/Docs.jsx
+++ client/Components/Docs.jsx
@@ -7,22 +7,24 @@ function Docs() {
     <div id="docs">
       <div className="step">Documentation</div>
       <br />
-      BeamQL is an open source tool to make understanding and adding a
-      GraphQL layer to any REST API as simple as possible.
-      <br></br>
-      <br></br>
-      The animation below shows the essential features of this tool. Simply
-      insert your PostgreSQL database URI or select from one of the sample
-      databases. Click submit and behold the visualization of your database as
-      a graph data structure. You are now looking at your data the way GraphQL
-      sees it.
-      <br></br>
-      <br></br>
-      In addition, BeamQL generates everything you will need (all schema and
-      resolvers) to add a GraphQL controller layer to your existing REST API.
-      Assuming you are using a common backend framework in Node.js, you simply
-      need to copy these two bodies of text into your server/app.js code, and
-      install and include the relevant boilerplate for<a id="textlink" href="https://www.npmjs.com/package/apollo-server">Apollo Server.</a>
+      <div id="docsTextOne">
+        BeamQL is an open source tool to make understanding and adding a
+        GraphQL layer to any REST API as simple as possible.
+        <br></br>
+        <br></br>
+        The animation below shows the essential features of this tool. Simply
+        insert your PostgreSQL database URI or select from one of the sample
+        databases. Click submit and behold the visualization of your database as
+        a graph data structure. You are now looking at your data the way GraphQL
+        sees it.
+        <br></br>
+        <br></br>
+        In addition, BeamQL generates everything you will need (all schema and
+        resolvers) to add a GraphQL controller layer to your existing REST API.
+        Assuming you are using a common backend framework in Node.js, you simply
+        need to copy these two bodies of text into your server/app.js code, and
+        install and include the relevant boilerplate for <a id="textlink" style={{"display": "inline"}} href="https://www.npmjs.com/package/apollo-server">Apollo Server</a>.
+      </div>
       <br />
       <br />
     <img
@@ -34,26 +36,17 @@ function Docs() {
     <br />
     <span className="step">Step-By-Step Migration 🔢</span>
     <ol id="olCode">
-      <li>Go to BeamQL.com</li>
+      <li>Copy your PostgreSQL database URI into BeamQL.com</li>
       <br/>
-      <li>Paste your PostgreSQL database URI into the input box</li>
+      <li>View and interact with the graphical visualization of the database and or use it in API documentation to give users a better understanding of what they are working with.</li>
       <br/>
-      <li>View and interact with the graphical visualization of your database and or use it 
-        in your API documentation to give your users a better understanding of what they are working with.</li>
+      <li>Open NodeJS backend codebase in a code editor.</li>
         <br/>
-      <li>Open your NodeJS backend codebase in a code editor.</li>
+      <li>Use the schema and resolvers that BeamQL produces with Apollo Server or a similar GraphQL server that suits you.</li>
       <br/>
-      <li>npm install apollo-server-express [or appropriate package for your backend framework]</li>
+      <li>Start server and navigate to localhost:X000/graphql</li>
       <br/>
-      <li>Copy boilerplate from Apollo Server’s quick start guide</li>
-      <br/>
-      <li>Copy the schema and resolvers output by BeamQL to your server.js/app.js file and pass them as schema and resolver arguments to your Apollo Server configuration.</li>
-      <br/>
-      <li>Start your server</li>
-      <br/>
-      <li>Navigate to localhost:X000/graphql</li>
-      <br/>
-      <li> the awesomeness of your database accessible through the query language of the future!</li>
+      <li>Behold the glory of your database accessible through the query language of the future!</li>
     </ol>
     <br />
     <br />    
